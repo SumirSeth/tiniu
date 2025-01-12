6b43458073c65ec24646acab7e65ee759b9b1105
Sumir Seth <iamsumirseth@gmail.com>
2025-01-12 23:24:16 +0530

    Update Supabase configuration to use public environment variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
+    url: process.env.NUXT_PUBLIC_SUPABASE_URL,
+    key: process.env.NUXT_PUBLIC_SUPABASE_KEY,
     redirect: false,
   },
   modules: [
